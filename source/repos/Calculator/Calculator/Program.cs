08ac95277ae8c4d7d79825932934625b982ba35e
Chris Sweeney <Christopher.Sweeney@IRIS.CO.UK>
2021-11-09 16:01:55 +0000

    Added ability to calculate multiple numbers

--- source/repos/Calculator/Calculator/Program.cs
+++ source/repos/Calculator/Calculator/Program.cs
@@ -11,38 +11,47 @@ namespace Calculator
             Console.WriteLine("Enter the operator: ");
             string operatorInput = Console.ReadLine();
 
-            Console.Write("Enter the first number: ");
-            string firstInput = Console.ReadLine();
-            // Parsing user string to an int for calculation
-            int firstNumber = int.Parse(firstInput);
+            string operatorWord = "";
 
+            Console.WriteLine("How many numbers would you like to " + operatorWord + "?");
+            int numberInput = int.Parse(Console.ReadLine());
 
-            Console.Write("Enter the second number: ");
-            string secondInput = Console.ReadLine();
-            // Parsing user string to an int for calculation
-            int secondNumber = int.Parse(secondInput);
+            int[] numbers = new int[numberInput];
+            for (int index = 0; index < numberInput; index++)
+            {
+                Console.Write("Please enter number " + (index + 1) + ": ");
+                numbers[index] = int.Parse(Console.ReadLine());
+            }
 
             int result = 0;
 
-            if (operatorInput == "+")
+            // Used sample solution to help with finishing this exercise
+            for (int index = 1; index < numberInput; index++)
             {
-                result = firstNumber + secondNumber;
-            }
-            else if (operatorInput == "-")
-            {
-                result = firstNumber - secondNumber;
-            }
-            else if (operatorInput == "*")
-            {
-                result = firstNumber * secondNumber;
-            }
-            else if (operatorInput == "/")
-            {
-                result = firstNumber / secondNumber;
-            }
-            else
-            {
-                Console.WriteLine("Please input a valid operator.");
+                if (operatorInput == "+")
+                {
+                    result = result + numbers[index];
+                    operatorWord = "add";
+                }
+                else if (operatorInput == "-")
+                {
+                    result = result - numbers[index];
+                    operatorWord = "subtract";
+                }
+                else if (operatorInput == "*")
+                {
+                    result = result * numbers[index];
+                    operatorWord = "multiply";
+                }
+                else if (operatorInput == "/")
+                {
+                    result = result - numbers[index];
+                    operatorWord = "divide";
+                }
+                else
+                {
+                    Console.WriteLine("Please input a valid operator.");
+                }
             }
 
             Console.WriteLine("Your result is: " + result);
