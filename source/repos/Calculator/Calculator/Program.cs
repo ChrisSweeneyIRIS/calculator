e9c5f0e36f1c73bf89afcd3b9d322fbd825f1d89
Chris Sweeney <Christopher.Sweeney@IRIS.CO.UK>
2021-11-09 10:03:07 +0000

    Added multiplication functionality

--- source/repos/Calculator/Calculator/Program.cs
+++ source/repos/Calculator/Calculator/Program.cs
@@ -7,7 +7,22 @@ namespace Calculator
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the calculator!");
-            string aFriend = Console.ReadLine();
+
+            Console.Write("Enter the first number: ");
+            string firstInput = Console.ReadLine();
+            // Parsing user string to an int for calculation
+            int firstNumber = int.Parse(firstInput);
+
+
+            Console.Write("Enter the second number: ");
+            string secondInput = Console.ReadLine();
+            // Parsing user string to an int for calculation
+            int secondNumber = int.Parse(secondInput);
+
+            int result = firstNumber * secondNumber;
+            Console.WriteLine("Your result is: " + result);
+
+            Console.ReadLine();
         }
     }
 }
